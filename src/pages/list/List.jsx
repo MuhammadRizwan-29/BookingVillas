30f63967ebc575e6365e0784d3c70164e5bd7a3e
Muhammad Rizwan <118816931+MuhammadRizwan-29@users.noreply.github.com>
2023-03-07 18:24:57 +0500

    Search List Created

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,11 +1,18 @@
 import Header from "../../components/header/Header";
 import Navbar from "../../components/navbar/Navbar";
+import "./List.css"
 
 export default function List(){
     return(
         <div>
             <Navbar />
             <Header type="list" />
+            <div className="listContainer">
+                <div className="listWrapper">
+                    <div className="listSerach"></div>
+                    <div className="listResult"></div>
+                </div>
+            </div>
         </div>
     );
 }
